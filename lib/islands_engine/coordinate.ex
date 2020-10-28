2cc52c5a5a1a53186b3e6b09b7b73b1e4f546df6
Stefano Franzin <stefano.franzin@gmail.com>
2020-10-28 20:08:44 +0100

    Remaining entities

--- lib/islands_engine/coordinate.ex
+++ lib/islands_engine/coordinate.ex
@@ -6,7 +6,8 @@ defmodule IslandsEngine.Coordinate do
 
   @board_range 1..10
 
-  def new(row, col) when row in(@board_range) and col in(@board_range), do:
-    {:ok, %Coordinate{row: row, col: col}}
+  def new(row, col) when row in @board_range and col in @board_range,
+    do: {:ok, %Coordinate{row: row, col: col}}
+
   def new(_row, _col), do: {:error, :invalid_coordinate}
 end
